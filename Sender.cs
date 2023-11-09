49a7716e1ff1afec6c4a9bc967baa1ae901d52a6
Shalvex Novachrono <55297590+ShalvexNovachrono@users.noreply.github.com>
2023-11-09 20:41:43 +0000

    Update Sender.cs

--- Sender.cs
+++ Sender.cs
@@ -1,3 +1,10 @@
+// Decompiled with JetBrains decompiler
+// Type: Sender
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: B34C14AB-0760-406D-ABE6-62CD929EE8CE
+// Assembly location: *//*
+// Compiler-generated code is shown
+
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,11 +14,15 @@ public class Sender : MonoBehaviour
 
   private void Update()
   {
-    if (!Input.GetKeyDown(KeyCode.Return) && !(MessageInput.text != ""))
+    if (!Input.GetKeyDown(KeyCode.Return) || !(this.MessageInput.text != ""))
       return;
-    Gameobject.FindAnyObjectByType<WebSocketWorker>().SendMessageParameter("", MessageInput.text);
-    MessageInput.text = "";
-    MessageInput.Select();
+    Object.FindAnyObjectByType<WebSocketWorker>().SendMessageParameter((string) null, this.MessageInput.text);
+    this.MessageInput.text = "";
+    this.MessageInput.Select();
   }
 
+  public Sender()
+  {
+    base.\u002Ector();
+  }
 }
